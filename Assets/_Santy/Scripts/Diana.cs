a74541710dd2e7825ccb743ba1ebf6f44cfe7f2b
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-09-04 23:25:58 -0600

    Final del juego

--- Assets/_Santy/Scripts/Diana.cs
+++ Assets/_Santy/Scripts/Diana.cs
@@ -1,16 +1,30 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Diana : MonoBehaviour
 {
+    private bool _able;
+    private void Start()
+    {
+        _able = false;
+    }
+
+    private void Update()
+    {
+        if (_able == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Bullet"))
-        {
-            Debug.Log(other.gameObject.name);
-            this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-        }
+        StartCoroutine(DisableCoroutine());
+    }
+
+    private IEnumerator DisableCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        _able = true;
     }
 }
