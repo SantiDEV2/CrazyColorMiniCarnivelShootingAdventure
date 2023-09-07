67ec164c1a76da35b8b82cd6fa08de2642b5afd7
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-09-06 21:30:45 -0600

    Changes

--- Assets/_Santy/Scripts/Sniper.cs
+++ Assets/_Santy/Scripts/Sniper.cs
@@ -19,4 +19,12 @@ public class Sniper : MonoBehaviour
             bullet.GetComponent<Rigidbody>().AddForce(-shotpoint.forward * force,ForceMode.Impulse);
         }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Diana"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
